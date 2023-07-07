459605289deaab2aca9f3209aa31bbe7241d9672
AlexisAguilera7 <alexisagylera@gmail.com>
2023-07-07 17:39:12 -0300

    recatjs

--- my-app/src/componentes /NavBar.js
+++ my-app/src/componentes /NavBar.js
@@ -1,16 +1,15 @@
 import React from 'react';
-import CartWidget from './CartWidget';
 
 const NavBar = () => {
  return (
   <nav>
-   <div className="brand">Título de la tienda</div>
+   <div className="brand">Allbasketuy</div>
    <ul className="categories">
-    <li>Categoría 1</li>
-    <li>Categoría 2</li>
-    <li>Categoría 3</li>
+    <li>Camisetas</li>
+    <li>pelotas</li>
+    <li>zapatillas</li>
    </ul>
-   <CartWidget />
+
   </nav>
  );
 };
