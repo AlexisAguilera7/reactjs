3f330b19eee7c19256be9ea87b7de93d9417b43c
AlexisAguilera7 <alexisagylera@gmail.com>
2023-07-11 15:20:06 -0300

    recatjs

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,14 +1,13 @@
 import React from 'react';
 import NavBar from '../src/componentes /NavBar';
 import ItemList from '../src/componentes /ItemList';
-import CartWidget from '../src/componentes /CartWidget';
+
 import './index.css';
 
 const App = () => {
   return (
     <div>
       <NavBar />
-      <CartWidget />
       <ItemList greeting="¡Hola! Bienvenido a nuestra tienda." />
 
     </div>
--- my-app/src/componentes /NavBar.js
+++ my-app/src/componentes /NavBar.js
@@ -1,15 +1,16 @@
 import React from 'react';
+import CartWidget from './CartWidget';
 
 const NavBar = () => {
  return (
   <nav>
-   <div className="brand">Allbasketuy</div>
+   <div className="titulo">Allbasketuy</div>
    <ul className="categories">
     <li><p><a class="link-opacity-75-hover" href="#">Camisetas</a></p></li>
     <li><p><a class="link-opacity-75-hover" href="#">Pelotas</a></p></li>
     <li><p><a class="link-opacity-75-hover" href="#">Zapatillas</a></p></li>
    </ul>
-
+   <CartWidget />
   </nav>
  );
 };
