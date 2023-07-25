97382c95637ab1de199201602cdd7eb48832137d
AlexisAguilera7 <alexisagylera@gmail.com>
2023-07-25 19:01:23 -0300

    recatjs

--- my-app/src/componentes /ItemlistContainer.jsx
+++ my-app/src/componentes /ItemlistContainer.jsx
@@ -6,10 +6,10 @@ import IemList from '../componentes /ItemList';
 const ItemListContainer = ({ greeting }) => {
   const [productos, setProductos] = useState([])
   const productList = [
-    { id: `01`, name: `Pelota`, stock: 10, precio: 500, description: `Pelota Spanding`, img: `../img/pelota.jpeg` }
-    { id: `02`, name: `Camiseta`, stock: 7, precio: 900, description: `Camiseta de los Angeles Lakers`, img: `../img/camiseta.jpeg` }
-    { id: `03`, name: `Tableros`, stock: 24, precio: 1500, description: `Tablero Spanding profesional`, img: `../img/tableros.jpeg` }
-    { id: `04`, name: `Zapatillas`, stock: 14, precio: 3000, description: `Nike Lebron18 low`, img: `../img/zapatillas.jpeg` }
+    { id: `01`, name: `Pelota`, stock: 10, precio: 500, description: `Pelota Spanding`, img: `../img/pelota.jpeg` },
+    { id: `02`, name: `Camiseta`, stock: 7, precio: 900, description: `Camiseta de los Angeles Lakers`, img: `../img/camiseta.jpeg` },
+    { id: `03`, name: `Tableros`, stock: 24, precio: 1500, description: `Tablero Spanding profesional`, img: `../img/tableros.jpeg` },
+    { id: `04`, name: `Zapatillas`, stock: 14, precio: 3000, description: `Nike Lebron18 low`, img: `../img/zapatillas.jpeg` },
   ]
 
 
