9a35aad95186f7eadc23e93d2a8ef55079341381
AlexisAguilera7 <alexisagylera@gmail.com>
2023-07-26 18:07:29 -0300

    recatjs4

--- my-app/src/componentes /ItemDetail.jsx
+++ my-app/src/componentes /ItemDetail.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import ItemCount from "./ItemCount";
-
+import { Card } from "react-bootstrap";
 
 const ItemDetail = ({ producto }) => {
  const onAdd = (cantidad) => {
@@ -10,8 +10,19 @@ const ItemDetail = ({ producto }) => {
 
  return (
   <div>
-   <h3>{producto.name}</h3>
-   <ItemCount initial={1} stock={producto.stock} onAdd={onAdd} />
+   <Card style={{ width: '18rem' }}>
+    <Card.Img variant="top" src={producto.img} />
+    <Card.Body>
+     <Card.Title>{producto.name}</Card.Title>
+     <Card.Text>
+      {producto.description}
+     </Card.Text>
+     <Card.Text>${producto.price}</Card.Text>
+    </Card.Body>
+    <h3>{producto.name}</h3>
+    <ItemCount initial={1} stock={producto.stock} onAdd={onAdd} />
+   </Card>
+
   </div>
  )
 }
--- my-app/src/mock/data.js
+++ my-app/src/mock/data.js
@@ -2,11 +2,11 @@ const productList = [
  { id: '01', category: 'Pelotas', name: 'Pelota', stock: 10, precio: 500, description: 'Pelota Spanding', img: '../img/pelota.jpeg' },
  { id: '02', category: 'Camisetas', name: 'Camiseta', stock: 7, precio: 900, description: 'Camiseta de los Angeles Lakers', img: '../img/camiseta.jpeg' },
  { id: '03', category: 'Tableros', name: 'Tableros', stock: 24, precio: 1500, description: 'Tablero Spanding profesional', img: '../img/tableros.jpeg' },
- { id: '04', category: 'Zapatillas', name: 'Zapatillas', stock: 14, precio: 3000, description: 'Nike Lebron18 low', img: '../ img / zapatillas.jpeg' },
+ { id: '04', category: 'Zapatillas', name: 'Zapatillas', stock: 14, precio: 3000, description: 'Nike Lebron18 low', img: '../img/zapatillas.jpeg' },
 ]
 
 export const getProducts = () => {
- new Promise((resolve, reject) => {
+ return new Promise((resolve, reject) => {
   let error = false
   setTimeout(() => {
    if (error) {
@@ -25,5 +25,4 @@ export const getItem = (id) => {
   }, 2000)
  })
 }
-
 
